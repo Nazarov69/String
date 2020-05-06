cb7f40441b6e63716e00c7825bc07211a8cf8e26
Nazarov69 <61185136+Nazarov69@users.noreply.github.com>
2020-05-06 21:46:32 +0300

    Add files via upload

--- String.cpp
+++ String.cpp
@@ -1,34 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class String
-{
-	int length;
-	char* str;
-public:
-	String();
-	String(const char* str);
-	String(String& a);
-	char* GetStr();
-	int GetLength();
-	void SetStrLength(const char* ch);
-	String operator+ (const String& a);
-	String& operator= (const String& a);
-	bool operator== (const String& a);
-	bool operator< (const String& a);
-	bool operator> (const String& a);
-	char& operator[] (int index);
-	friend ostream& operator<< (ostream& os, const String& a);
-	friend istream& operator>> (istream& is, String& a);
-	~String();
-	int Length();
-	void Print();
-	char* Find(const char* tmp);
-	char* FirstFind(const char* ch);
-	void Getline(const char* ch);
-};
-
+#include "String.h"
 String::String() {
 	str = nullptr;
 	length = 0;
@@ -36,19 +6,26 @@ String::String() {
 String::String(const char* str) {
 	length = strlen(str);
 	this->str = new char[length + 1];
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++) 
 		this->str[i] = str[i];
-	}
 	this->str[length] = '\0';
+
+}
+
+String::String(char st, int length) {
+	this->length = length + 1;
+	str = new char[length + 1];
+	for (int i = 0; i < length; i++)
+		str[i] = st;
+	str[length] = '\0';
 
 }
 
 String::String(String& a) {
 	length = strlen(a.str);
-	this->str = new char[length + 1];
-	for (int i = 0; i < length; i++) {
+	str = new char[length + 1];
+	for (int i = 0; i < length; i++) 
 		this->str[i] = a.str[i];
-	}
 	this->str[length] = '\0';
 }
 
@@ -61,10 +38,9 @@ int String::GetLength() {
 
 void String::SetStrLength(const char* ch) {
 	length = strlen(ch);
-	this->str = new char[length + 1];
-	for (int i = 0; i < length; i++) {
-		this->str[i] = ch[i];
-	}
+	str = new char[length + 1];
+	for (int i = 0; i < length; i++) 
+		str[i] = ch[i];
 	str[length] = '\0';
 }
 
@@ -75,12 +51,10 @@ String String::operator+ (const String& a) {
 	NewStr.length = thisLength + aLength;
 	NewStr.str = new char[thisLength + aLength + 1];
 	int i = 0;
-	for (; i < thisLength; i++) {
+	for (; i < thisLength; i++) 
 		NewStr.str[i] = this->str[i];
-	}
-	for (int j = 0; j < aLength; j++, i++) {
+	for (int j = 0; j < aLength; j++, i++) 
 		NewStr.str[i] = a.str[j];
-	}
 	NewStr.str[thisLength + aLength] = '\0';
 	return NewStr;
 }
@@ -91,9 +65,8 @@ String& String::operator= (const String& a) {
 	}
 	length = strlen(a.str);
 	this->str = new char[length + 1];
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++) 
 		this->str[i] = a.str[i];
-	}
 	this->str[length] = '\0';
 	return*this;
 }
@@ -115,16 +88,16 @@ bool String::operator< (const String& a) {
 }
 
 bool String::operator> (const String& a) {
-	return length < a.length;
+	return length > a.length;
 }
 char& String::operator[] (int index) {
+	if (index >= 0 && index < length) 
 	return this->str[index];
 }
 
 ostream& operator<< (ostream& os, const String& a) {
-	for (int i = 0; i < a.length; i++) {
+	for (int i = 0; i < a.length; i++) 
 		os << a.str[i];
-	}
 	return os;
 }
 
@@ -137,15 +110,7 @@ istream& operator>> (istream& is, String& a) {
 }
 
 String::~String() {
-	delete[] this->str;
-}
-
-int String::Length() {
-	return length;
-}
-
-void String::Print() {
-	cout << str << endl;
+	delete[] str;
 }
 
 char* String::Find(const char* tmp) {
@@ -158,43 +123,23 @@ char* String::FirstFind(const char* ch) {
 	return c;
 }
 
-void String::Getline(const char* ch) {
-	char* c = strtok(str, ch);
+String* String::Getline(const char* ch) {
+	String tmp = str;
+	int digit = 0;
+	char* c = strtok(tmp.str, ch);
 	while (c != NULL) {
-		cout << c << endl;
+		digit += 1;
 		c = strtok(NULL, ch);
 	}
-}
+	tmp = c;
+	String* a = new String[digit];
+	digit = 0;
+	c = strtok(tmp.str, ch);
+	while (c != NULL){
+		a[digit] = c;
+		digit += 1;
+		c = strtok(NULL, c);
+	}
 
-int main() {
-	String str;
-	String str2("Hello");
-	String str3(str2);
-	cout << str2.GetStr() << endl;
-	cout << str3.GetStr() << endl;
-	str3.SetStrLength("World");
-	cout << str3 << endl;
-	String result;
-	result = str2 + str3;
-	bool equal = str2 == str3;
-	cout << "+ : " << result << endl;
-	cout << "== : " << equal << endl;
-	cout << str2.Length() << endl;
-	cout << str3.Length() << endl;
-	cout << result.Length() << endl;
-	String str4("IITMM FIIT");
-	String str5("IITMM FIIT");
-	bool equal2 = str4 == str5;
-	cout << "== : " << equal2 << endl;
-	str5.SetStrLength("FIIT");
-	bool res = str4 < str5;
-	cout << "< : " << res << endl;
-	bool res2 = str4 > str5;
-	cout << "> : " << res2 << endl;
-	cout << "[] : " << str4[0] << endl;
-	cout << str4.Find("I") << endl;
-	cout << str4.FirstFind("T") << endl;
-	str4.Getline(" ");
-
-	return 0;
+	return a;
 }
